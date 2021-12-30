8503e372429b0351fd8bf040e953c04b29089a22
张高磊 <mrglzh@yeah.net>
2021-12-30 17:46:33 +0800

    Initial Commit

---
+++ IFUIKit/IFUIKit.h
@@ -0,0 +1,18 @@
+//
+//  IFUIKit.h
+//  IFUIKit
+//
+//  Created by MrGLZh on 2021/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IFUIKit.
+FOUNDATION_EXPORT double IFUIKitVersionNumber;
+
+//! Project version string for IFUIKit.
+FOUNDATION_EXPORT const unsigned char IFUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFUIKit/PublicHeader.h>
+
+
